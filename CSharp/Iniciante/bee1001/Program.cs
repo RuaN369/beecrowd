fd3aae610bc681ed8d7454f974af96414f5ed162
Ruan Peixoto <ruanbpeixoto@gmail.com>
2022-01-14 23:17:59 -0300

    Update Program.cs

--- CSharp/Iniciante/bee1001/Program.cs
+++ CSharp/Iniciante/bee1001/Program.cs
@@ -1,11 +1,20 @@
 using System;
 
-namespace bee1000
+namespace bee1001
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            int A, B, X;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            X = A + B;
+
+            Console.WriteLine($"X = {X}");
 
         }
     }
