7ff0a156415e488a6bc90e0f8616642e8f4fc1e7
Ruan Peixoto <ruanbpeixoto@gmail.com>
2022-01-14 21:47:53 -0300

    Add files via upload

---
+++ bee1001/bee1001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bee1001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
