75f7eed7d675a659c5f9c16df697d1d933aa87a5
Ruan Peixoto <ruanbpeixoto@gmail.com>
2022-01-14 23:57:44 -0300

    Create Program.cs

---
+++ CSharp/Iniciante/bee1005/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Globalization;
+
+namespace bee1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double A, B , media;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = (A * 3.5 + B * 7.5) / 11;
+
+            Console.WriteLine("MEDIA = " + media.ToString("F5", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
