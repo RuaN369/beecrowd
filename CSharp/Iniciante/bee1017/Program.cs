a8471c12a980adcbe814f8dcedf911a22c922ea4
Ruan Peixoto <ruanbpeixoto@gmail.com>
2022-01-24 19:48:41 -0300

    Update Program.cs

--- CSharp/Iniciante/bee1017/Program.cs
+++ CSharp/Iniciante/bee1017/Program.cs
@@ -1,1 +1,24 @@
+using System;
+using System.Globalization;
 
+namespace bee1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double A, B, X;
+
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            X = A * B / 12;
+
+            Console.WriteLine(X.ToString("F3", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+
+        }
+    }
+}
