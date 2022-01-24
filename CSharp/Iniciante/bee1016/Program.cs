c8e01c0f841b630a95292a2321e15a0a93af40da
Ruan Peixoto <ruanbpeixoto@gmail.com>
2022-01-24 01:57:28 -0300

    Create Program.cs

---
+++ CSharp/Iniciante/bee1016/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+
+namespace bee1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+
+            a = a * 2;
+
+            Console.WriteLine($"{a} minutos");
+
+        }
+    }
+}
