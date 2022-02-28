eafd47d95a4c2f2c6364eb9c81d99090747d83ac
Ruan Peixoto <ruanbpeixoto@gmail.com>
2022-02-28 10:41:07 -0300

    Update Program.cs

--- CSharp/Iniciante/bee1052/Program.cs
+++ CSharp/Iniciante/bee1052/Program.cs
@@ -7,57 +7,53 @@ namespace bee1052
     {
         static void Main(string[] args)
         {
-            int mes = int.Parse(Console.ReadLine());
-            
-            if (mes == 1)
+            int x = int.Parse(Console.ReadLine());
+            string mes;
+
+            switch (x)
             {
-                Console.WriteLine("January");
-            }
-            else if (mes == 2)
-            {
-                Console.WriteLine("February");
-            }
-            else if (mes == 3)
-            {
-                Console.WriteLine("March");
-            }
-            else if (mes == 4)
-            {
-                Console.WriteLine("April");
-            }
-            else if (mes == 5)
-            {
-                Console.WriteLine("May");
-            }
-            else if (mes == 6)
-            {
-                Console.WriteLine("June");
-            }
-            else if (mes == 7)
-            {
-                Console.WriteLine("July");
-            }
-            else if (mes == 8)
-            {
-                Console.WriteLine("August");
-            }
-            else if (mes == 9)
-            {
-                Console.WriteLine("September");
-            }
-            else if (mes == 10)
-            {
-                Console.WriteLine("October");
-            }
-            else if (mes == 11)
-            {
-                Console.WriteLine("November");
-            }
-            else if (mes == 12)
-            {
-                Console.WriteLine("December");
+                case 1:
+                    mes = "January";
+                    break;
+                case 2:
+                    mes = "February";
+                    break;
+                case 3:
+                    mes = "March";
+                    break;
+                case 4:
+                    mes = "April";
+                    break;
+                case 5:
+                    mes = "May";
+                    break;
+                case 6:
+                    mes = "June";
+                    break;
+                case 7:
+                    mes = "July";
+                    break;
+                case 8:
+                    mes = "August";
+                    break;
+                case 9:
+                    mes = "September";
+                    break;
+                case 10:
+                    mes = "October";
+                    break;
+                case 11:
+                    mes = "November";
+                    break;
+                case 12:
+                    mes = "December";
+                    break;
+                default:
+                    mes = "Valor Inválido";
+                    break;
             }
 
+            Console.WriteLine(mes);
             Console.ReadKey();
 
         }
