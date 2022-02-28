f1c0e9422ae5628b883edce15f855097991ffaf8
Ruan Peixoto <ruanbpeixoto@gmail.com>
2022-02-28 08:59:24 -0300

    Update Program.cs

--- CSharp/Iniciante/bee1036/Program.cs
+++ CSharp/Iniciante/bee1036/Program.cs
@@ -15,7 +15,7 @@ namespace bee1036
             double B = double.Parse(vet[1], CultureInfo.InvariantCulture);
             double C = double.Parse(vet[2], CultureInfo.InvariantCulture);
 
-            delta = (B * B) - (4 * A * C);
+            delta = Math.Pow(B, 2) - (4 * A * C);
             double raiz = Math.Sqrt(delta);
             var a1 = ((-(B)) + raiz) / (2 * A);
             var a2 = ((-(B)) - raiz) / (2 * A);
@@ -23,15 +23,12 @@ namespace bee1036
             if (a1 <= 0)
             {
 
-                Console.WriteLine("R1 = " + a1.ToString("F5", CultureInfo.InvariantCulture));
-                Console.WriteLine("R2 = " + a2.ToString("F5", CultureInfo.InvariantCulture));
-                Console.ReadKey();
-
+                Console.WriteLine("R1 = {0}", a1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = {0}", a2.ToString("F5", CultureInfo.InvariantCulture));
             }
             else
             {
                 Console.WriteLine("Impossivel calcular");
-                Console.ReadKey();
             }
 
             Console.ReadKey();
